e7a0b2603a13626ecd247adb0b1f8e7445686488
damimi88 <hhf505230@gmail.com>
2025-06-08 05:20:21 -0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-// 读取并执行 auto-follow.js 文件
-const autoFollowScript = fs.readFileSync(path.join(__dirname, 'auto-follow.js'), 'utf8');
-
-// 运行脚本
-eval(autoFollowScript);  // 注意：使用 eval 会执行代码，但要小心潜在的安全风险
